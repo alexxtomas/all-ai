c35dd7073930484680f5116e7904392da1c0b474
Alex Tomas <alex.tomas@thepowermba.com>
2023-08-12 17:54:45 +0200

    chore: Use MobileSidebar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,15 +1,12 @@
-import { Button } from '@/components/ui/button'
 import { UserButton } from '@clerk/nextjs'
-import { Menu } from 'lucide-react'
+import MobileSidebar from './mobile-sidebar'
 
 export default function Navbar() {
   return (
     <div className='flex items-center p-4'>
-      <Button variant={'ghost'} size={'icon'} className='sm:hidden'>
-        <Menu />
-      </Button>
+      <MobileSidebar />
       <div className='flex w-full justify-end'>
-        <UserButton afterSignOutUrl='/' />
+        <UserButton afterSignOutUrl='/' />M
       </div>
     </div>
   )
