9f34cfee9f80fa1ff5f0bc887b107e8b8c462908
Alex Tomas <alex.tomas@thepowermba.com>
2023-08-12 17:54:59 +0200

    chore: Use SideBar component

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,10 +1,11 @@
 import Navbar from '@/components/navbar'
+import Sidebar from '@/components/sidebar'
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className='h-full relative'>
       <aside className='hidden h-full sm:w-72 sm:flex sm:flex-col sm:fixed sm:inset-y-0 z-[80] bg-gray-900'>
-        <div>Hello</div>
+        <Sidebar />
       </aside>
       <main className='sm:pl-72'>
         <Navbar />
