b692bc5366d2e35d18839208938b508cba56e177
alexxtomas <atomasllimos@gmail.com>
2023-08-16 15:47:08 +0200

    feat: Create TOOLS constant

--- utils/constants.ts
+++ utils/constants.ts
@@ -40,19 +40,55 @@ export const ROUTES = [
     icon: Music,
     color: 'text-emerald-500'
   },
-  ,
   {
     label: 'Code Generation',
     href: '/code',
     icon: Code,
     color: 'text-green-700'
   },
-  ,
   {
     label: 'Settings',
     href: '/settings',
     icon: Settings,
     color: ''
+  }
+]
+
+export const TOOLS = [
+  {
+    label: 'Conversation',
+    icon: MessageSquare,
+    color: 'text-violet-500',
+    bgColor: 'bg-violet-500/10',
+    href: '/conversation'
+  },
+  {
+    label: 'Music Generation',
+    icon: Music,
+    color: 'text-emerald-500',
+    bgColor: 'bg-emerald-500/10',
+    href: '/music'
+  },
+  {
+    label: 'Image Generation',
+    icon: ImageIcon,
+    color: 'text-pink-500',
+    bgColor: 'bg-pink-500/10',
+    href: '/image'
+  },
+  {
+    label: 'Video Generation',
+    icon: VideoIcon,
+    color: 'text-orange-500',
+    bgColor: 'bg-orange-500/10',
+    href: '/video'
+  },
+  {
+    label: 'Code Generation',
+    icon: Code,
+    color: 'text-green-700',
+    bgColor: 'bg-green-500/10',
+    href: '/code'
   }
 ]
 
