7eaaa93dfb60d0cc363b6b50b0495a6542d6cb47
alexxtomas <atomasllimos@gmail.com>
2023-08-16 15:44:52 +0200

    feat: Create Dashboard Page

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,5 +1,20 @@
-import { UserButton } from '@clerk/nextjs'
+import LinkCard from '@/components/link-card'
+import { TOOLS } from '@/utils/constants'
 
 export default function DashboardPage() {
-  return <div></div>
+  return (
+    <section>
+      <header className='mb-8 space-y-4'>
+        <h2 className='text-2xl md:text-4xl font-bold text-center'> Explore the power of AI</h2>
+        <p className='text-muted-foreground font-light text-sm md:text-lg text-center'>
+          Chat with the smartest AI - Experience the power of AI
+        </p>
+      </header>
+      <div className='px-4 md:px-20 lg:px-32 space-y-4'>
+        {TOOLS.map((tool) => {
+          return <LinkCard key={tool.href} {...tool} />
+        })}
+      </div>
+    </section>
+  )
 }
