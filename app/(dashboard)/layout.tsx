26584a6d44dbb5ac1982845475fa4015fd7cc9af
alexxtomas <atomasllimos@gmail.com>
2023-08-17 19:14:22 +0200

    feat: getApiLimitCout function

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,11 +1,13 @@
 import Navbar from '@/components/navbar'
 import Sidebar from '@/components/sidebar'
+import { getApiLimitCount } from '@/lib/api-limit'
 
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
+  const apiLimitCount = await getApiLimitCount()
   return (
     <div className='h-full relative'>
-      <aside className='hidden h-full md:w-72 md:flex md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900'>
-        <Sidebar />
+      <aside className='hidden h-full md:w-72 md:flex md:flex-col md:fixed md:inset-y-0 bg-gray-900'>
+        <Sidebar apiLimitCount={apiLimitCount} />
       </aside>
       <main className='md:pl-72'>
         <Navbar />
