99123202b77c5be368685d1efb802ea9e7510b31
alexxtomas <atomasllimos@gmail.com>
2023-08-17 16:41:18 +0200

    chore: Delete constant and add other

--- utils/constants.ts
+++ utils/constants.ts
@@ -54,42 +54,6 @@ export const ROUTES = [
   }
 ]
 
-export const TOOLS = [
-  {
-    label: 'Conversation',
-    icon: MessageSquare,
-    color: 'text-violet-500',
-    bgColor: 'bg-violet-500/10',
-    href: '/conversation'
-  },
-  {
-    label: 'Music Generation',
-    icon: Music,
-    color: 'text-emerald-500',
-    bgColor: 'bg-emerald-500/10',
-    href: '/music'
-  },
-  {
-    label: 'Image Generation',
-    icon: ImageIcon,
-    color: 'text-pink-500',
-    bgColor: 'bg-pink-500/10',
-    href: '/image'
-  },
-  {
-    label: 'Video Generation',
-    icon: VideoIcon,
-    color: 'text-orange-500',
-    bgColor: 'bg-orange-500/10',
-    href: '/video'
-  },
-  {
-    label: 'Code Generation',
-    icon: Code,
-    color: 'text-green-700',
-    bgColor: 'bg-green-500/10',
-    href: '/code'
-  }
-]
+export const MAX_FREE_COUNTS = 5
 
 export const MONTSERRAT = Montserrat({ weight: '600', subsets: ['latin'] })
