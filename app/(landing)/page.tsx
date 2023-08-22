a331e9613647b53cd0a5b8d1cd36f5a88a734b1b
alexxtomas <atomasllimos@gmail.com>
2023-08-22 15:57:28 +0200

    chore: Remove unnecessary import

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,10 +1,8 @@
-import LandingContent from '@/components/landing-content'
 import LandingHero from '@/components/landing-hero'
 export default function LandingPage() {
   return (
     <>
       <LandingHero />
-      {/* <LandingContent /> */}
     </>
   )
 }
